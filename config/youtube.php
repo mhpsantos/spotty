f22055b2983fb99c4234f7e0accf561bcc772437
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-09-25 18:16:04 -0300

    Delete youtube.php

--- config/youtube.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'client_id' => '894261976679-sl2gee4cmm2g9i3vbookhnbglr65bhrh.apps.googleusercontent.com',
-    'client_secret' => 'qYOEA376p-TPhfX9hYBF_1KG',
-    'redirect_uri' => 'http://localhost:8000/youtube/code'
-];
